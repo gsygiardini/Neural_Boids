f180d32c0b4495c809b88f29796d40b4f48f4105
GuilhermeGiardini <gsg95@nau.edu>
2024-04-14 03:29:34 -0700

    testing phase - added cpu order parameter...

--- herd.jl
+++ herd.jl
@@ -5,31 +5,144 @@ iniₚₐᵣₜₛ = 100
 
 parameters_dict = Dict(
     "total_steps" => 10000,          #Number of steps in the simulation
-    "maxₚₐᵣₜₛ" => 100,             #Maximum number of particles in the simulation
-    "avg_repr" => false,            #Crossover reproduction (true) vs Select best "genes"
-    "roulette" => false,            #Kill boids with weighted probabilities
+    "anim_steps" => 500,          #Number of steps in the animation
+    "nₚₐᵣₜₛ" => 100,               #Maximum number of particles in the simulation
+    "avg_repr" => false,           #Crossover reproduction (true) vs Select best "genes"
+    "roulette" => false,           #Kill boids with weighted probabilities
     "weighted_repr" => false,      #Reproduction has a chance to happen depending of fitness difference
     "reflect_walls" => false,      #Confine boids in a closed space(true) vs Periodic boundary condition (false)
-    "vision" => false,      #Boids can only perceive other boids at a certain direction
+    "vision" => false,             #Boids can only perceive other boids at a certain direction
     "potential" => false,          #Boids are penalized for touching each other
     "innertia" => false,           #Boids have rotational innertia(true)
     "fermionic" => false,          #Boids have a hard core(true)
     "box_size" => 1,               #Size of the reservoir where boids are in
     "ε" => 0.5,                    #DB scan parameter
     "min_pts" => 3,                #DB minimum number of points per cluster
-    "λ" => 0.0,                 #Rate of mutation
-    "β" => 250,                   #Boids vision distance center_of_mass .+= ((exp(- "β" *dist^2.0)/length(Boids)) * fov ) .* boid₂.pos
+    "μ" => 0.0,                    #Rate of mutation
+    "β" => 250,                    #Boids vision distance center_of_mass .+= ((exp(- "β" *dist^2.0)/length(Boids)) * fov ) .* boid₂.pos
     "γ" => 0.5,                    #Offspring rate multiplier
     "κ" => 50.0,                   #S curve to avoid boids touching coefficient 1/(1 - exp(-κ(x-x0)))
     "x₀" => 0.07,                  #S curve to avoid boids touching center value 1/(1 - exp(-κ(x-x0)))
-    "r" => 0.1,                   #Boid Interaction Radius
-    "rr" => 1.0,                  #Boid Reproduction Radius
+    "r" => 0.1,                    #Boid Interaction Radius
+    "rr" => 0.2,                   #Boid Reproduction Radius
     "cr" => 0.02,                  #Boid Collision Radius
     "cn" => 0,                     #Boid Child Number
     "neighbors" => Int32[],        #Boid Initial neighbors
     "speed" => 1.0,                #Boid Velocity
     "τ" => 0,                      #Boid Lifetime
+    "Δt" => 1e-2                   #
 )
+
+function update_boids!(parameters_dict::Dict, Δt::Float64)
+    nₚₐᵣₜₛ = parameters_dict["nₚₐᵣₜₛ"]
+    total_steps = parameters_dict["total_steps"]
+
+    d_params = [parameters_dict["nₚₐᵣₜₛ"],
+                 parameters_dict["total_steps"],
+                 parameters_dict["Δt"],
+                 parameters_dict["μ"],
+                 parameters_dict["β"],
+                 parameters_dict["κ"],
+                 parameters_dict["γ"],
+                 parameters_dict["x₀"],
+                 parameters_dict["box_size"],
+                 parameters_dict["vision"],
+                 parameters_dict["innertia"],
+                 parameters_dict["avg_repr"],
+                 parameters_dict["roulette"],
+                 parameters_dict["potential"],
+                 parameters_dict["fermionic"],
+                 parameters_dict["weighted_repr"],
+                 parameters_dict["reflect_walls"],
+                ] |> cu
+
+    d_r = Array{Float32}(undef, nₚₐᵣₜₛ)
+    d_τ = Array{Int32}(undef, nₚₐᵣₜₛ)
+    d_ϕ = Array{Float32}(undef, nₚₐᵣₜₛ)
+    d_cn = Array{Int32}(undef, nₚₐᵣₜₛ)
+    d_sx = Array{Bool}(undef, nₚₐᵣₜₛ)
+    d_rr = Array{Float32}(undef, nₚₐᵣₜₛ)
+    d_pos = Array{Float32, 2}(undef, 2, nₚₐᵣₜₛ)  # Assuming pos is a 2D array
+    d_θ = Array{Float32}(undef, nₚₐᵣₜₛ)
+    d_model = Vector{Chain}(undef, nₚₐᵣₜₛ)
+    d_speed = Array{Float32}(undef, nₚₐᵣₜₛ)
+    d_neighbors = Array{Int32, 2}(undef, 8, nₚₐᵣₜₛ)
+
+    for idx in 1:nₚₐᵣₜₛ
+        d_r[idx] = parameters_dict["r"]
+        d_τ[idx] = parameters_dict["τ"]
+        d_ϕ[idx] = Float32(rand())
+        d_cn[idx] = parameters_dict["cn"]
+        d_sx[idx] = rand(Bool)
+        d_rr[idx] = parameters_dict["rr"]
+        d_pos[:, idx] .= Float32.([2*rand()-1.0, 2*rand()-1.0])
+        d_θ[idx] = Float32(π * rand(-1.0:1e-5:1.0))
+        d_speed[idx] = parameters_dict["speed"]
+    end
+
+    d_r = d_r |> cu
+    d_τ = d_τ |> cu
+    d_ϕ = d_ϕ |> cu
+    d_θ = d_θ |> cu
+    d_cn = d_cn |> cu
+    d_sx = d_sx |> cu
+    d_rr = d_rr |> cu
+    d_pos = d_pos |> cu
+    d_speed = d_speed |> cu
+
+    inputₗₑₙ = 3
+    hiddenₗₑₙ = 3
+    outₗₑₙ = 1
+
+    W₁ = randn(Float32, nₚₐᵣₜₛ, hiddenₗₑₙ, inputₗₑₙ) |> cu
+    W₂ = randn(Float32, nₚₐᵣₜₛ, outₗₑₙ, hiddenₗₑₙ) |> cu
+
+    b₁ = randn(Float32, nₚₐᵣₜₛ, hiddenₗₑₙ) |> cu
+    b₂ = randn(Float32, nₚₐᵣₜₛ, outₗₑₙ) |> cu
+
+    out = zeros(Float32, nₚₐᵣₜₛ, 3) |> cu
+    x = zeros(Float32, nₚₐᵣₜₛ, hiddenₗₑₙ) |> cu
+    avg_coord = zeros(Float32, nₚₐᵣₜₛ, 3) |> cu
+
+    t = [1:total_steps/1000]
+    ψ = []
+
+    for τ in 1:total_steps
+        @cuda threads=nₚₐᵣₜₛ update_kernel!(d_params, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_speed, W₁, W₂, b₁, b₂, x, out, avg_coord)
+
+        if τ % 100 == 0
+            println("step: $τ of $total_steps")
+        end
+
+        if τ % 1000 == 0
+            push!(ψ,order_parameter(nₚₐᵣₜₛ, d_pos|>cpu, d_θ|>cpu, d_speed|>cpu))
+        end
+    end
+
+    println("Starting Animation")
+    anim_size = parameters_dict["anim_steps"]
+    anim = @animate for τ in 1:anim_size
+        @cuda threads=nₚₐᵣₜₛ update_kernel!(d_params, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_speed, W₁, W₂, b₁, b₂, x, out, avg_coord)
+
+        θ = d_θ |> cpu
+        pos = d_pos |> cpu
+
+        Plots.scatter(pos[1,:], pos[2,:], aspect_ratio=:equal, legend=false)
+        quiver!(pos[1,:], pos[2,:], quiver=(0.02*cos.(θ), 0.02*sin.(θ)), color=:blue)
+
+        xlims!(0, 1)
+        ylims!(0, 1)
+
+        if τ % 100 == 0
+            println("animation step: $τ of $anim_size")
+        end
+    end
+
+    gif(anim, "./results/boid_animation.mp4", fps = 10)
+
+    plot(t, ψ, xlims=(1,Inf), label="Order Parameter Over Time")
+    savefig("./results/order_param.png")
+end
 
 update_boids!(parameters_dict, 1e-2)
 #     Plots.scatter([b.pos[1] for b in updated_Boids], [b.pos[2] for b in updated_Boids], aspect_ratio=:equal, legend=false)
--- herd_functions.jl
+++ herd_functions.jl
@@ -10,183 +10,32 @@ using Statistics
 using Colors, ColorSchemes, PerceptualColourMaps
 using LinearAlgebra
 
-
-mutable struct Boid <: Function
-    model::Chain              #Boids Neural Network model
-    pos::Vector{Float32}   #Boid Position
-    θ::Float32                #Boid Orientation
-    speed::Float32            #Boid Velocity
-    ϕ::Float32                #Boid's Fitness (Child Number Over time)
-    τ::Int32                  #Boid's Lifetime
-    r::Float32                #Interaction Radius
-    sx::Bool                  #Boid "Sex"
-    rr::Float32               #Boid's Reproduction radius
-    cn::Int32                 #Boid's Child Number
-    index::Int32              #Boid Index
-    neighbors::Array{Int32} #Neighbor List
-
-    function Boid(parameters_dict::Dict,
-        model::Chain,
-        pos::Vector{Float32},
-        θ::Float32=Float32(π * rand(-1.0:1e-5:1.0)),               # Boid Orientation# =[2*rand()-1.0, 2*rand()-1.0],  # Boid Position
-        ϕ::Float32=Float32(rand()),
-        sx::Bool=rand(Bool)                                # Boid "sex"
-        )
-
-        index = 1
-        τ = Int64(parameters_dict["τ"])  # Convert to Int64
-        cn = Int64(parameters_dict["cn"])  # Convert to Int64
-
-        r = Float64(parameters_dict["r"])  # Convert to Float64
-        rr = Float64(parameters_dict["rr"])  # Convert to Float64
-        speed = Float64(parameters_dict["speed"])  # Convert to Float64
-
-#         pos = reshape(CUDA.CuArray(pos), :, 1)
-
-        neighbors = parameters_dict["neighbors"]
-
-        new(model, pos, θ, speed, ϕ, τ, r, sx, rr, cn, index, neighbors)
-    end
-end
-
-function create_boids(parameters_dict::Dict,n_particles::Int64)
-    Boids = Boid[]
-
-    for i in 1:n_particles
-        model = Chain(Dense(3, 3, tanh; init=Flux.glorot_uniform(gain=sqrt(2))),
-                Dense(3, 2, tanh; init=Flux.glorot_uniform(gain=sqrt(2))),
-                Dense(2, 1, tanh; init=Flux.glorot_uniform(gain=1))) |> cu
-
-        pos = Float32.([2*rand()-1.0, 2*rand()-1.0])
-        boid = Boid(parameters_dict, model, pos)
-        push!(Boids,boid)
-    end
-
-    return Boids
-end
-
-function update_boids!(parameters_dict::Dict, Δt::Float64)
-
-    nₚₐᵣₜₛ = 100#parameters_dict["maxₚₐᵣₜₛ"]
-    Boids = create_boids(parameters_dict,nₚₐᵣₜₛ)
-
-    total_steps = parameters_dict["total_steps"]
-
-#     d_params = Int32.([parameters_dict["vision"],
-#               parameters_dict["innertia"],
-#               parameters_dict["avg_repr"],
-#               parameters_dict["roulette"],
-#               parameters_dict["potential"],
-#               parameters_dict["fermionic"],
-#               parameters_dict["weighted_repr"],
-#               parameters_dict["reflect_walls"],
-#               ]) |> cu
-    # Transfer data to GPU memory
-
-    # Initialize CuArrays directly
-    d_i = Array{Int32}(undef, length(Boids))
-    d_r = Array{Float32}(undef, length(Boids))
-    d_τ = Array{Int32}(undef, length(Boids))
-    d_ϕ = Array{Float32}(undef, length(Boids))
-    d_cn = Array{Int32}(undef, length(Boids))
-    d_sx = Array{Bool}(undef, length(Boids))
-    d_rr = Array{Float32}(undef, length(Boids))
-    d_pos = Array{Float32, 2}(undef, 2, length(Boids))  # Assuming pos is a 2D array
-    d_coords = Array{Float32, 2}(undef, 3, length(Boids))  # Assuming pos is a 2D array
-    d_θ = Array{Float32}(undef, length(Boids))
-    d_model = Vector{Chain}(undef, length(Boids))
-    d_speed = Array{Float32}(undef, length(Boids))
-    d_neighbors = Array{Int32, 2}(undef, 8, length(Boids))
-
-    # Fill the CuArrays with data from Boids
-    for (i, boid) in enumerate(Boids)
-        d_i[i] = boid.index
-        d_r[i] = boid.r
-        d_τ[i] = boid.τ
-        d_ϕ[i] = boid.ϕ
-        d_cn[i] = boid.cn
-        d_sx[i] = boid.sx
-        d_rr[i] = boid.rr
-        d_pos[:, i] .= boid.pos
-        d_coords[:,i] .= vcat(boid.pos,[boid.θ])
-        d_θ[i] = boid.θ
-        d_model[i] = boid.model
-        d_speed[i] = boid.speed
-    end
-
-    d_i = d_i |> cu
-    d_r = d_r |> cu
-    d_τ = d_τ |> cu
-    d_ϕ = d_ϕ |> cu
-    d_θ = d_θ |> cu
-    d_cn = d_cn |> cu
-    d_sx = d_sx |> cu
-    d_rr = d_rr |> cu
-    d_pos = d_pos |> cu
-    d_speed = d_speed |> cu
-
-    inputₗₑₙ = 3
-    hiddenₗₑₙ = 3
-    outₗₑₙ = 1
-
-    W₁ = randn(Float32, nₚₐᵣₜₛ, hiddenₗₑₙ, inputₗₑₙ) |> cu
-    W₂ = randn(Float32, nₚₐᵣₜₛ, outₗₑₙ, hiddenₗₑₙ) |> cu
-
-    b₁ = randn(Float32, nₚₐᵣₜₛ, hiddenₗₑₙ) |> cu
-    b₂ = randn(Float32, nₚₐᵣₜₛ, outₗₑₙ) |> cu
-
-    cm = zeros(Float32, 2, 2) |> cu
-
-    x = zeros(Float32, inputₗₑₙ) |> cu
-
-    out = zeros(Float32, 3) |> cu
-
-    avg_coord = zeros(Float32, 3) |> cu
-
-    for τ in 1:total_steps
-        @cuda threads=length(Boids) update_kernel!(d_i, d_r, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_speed, W₁, W₂, b₁, b₂, cm, x, out, avg_coord)
-
-        if τ % 100 == 0
-            println("step: $τ of $total_steps")
-        end
-    end
-
-    println("Starting Animation")
-    anim_size = 1000
-    anim = @animate for τ in 1:anim_size
-        @cuda threads=length(Boids) update_kernel!(d_i, d_r, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_speed, W₁, W₂, b₁, b₂, cm, x, out, avg_coord)
-
-        θ = d_θ |> cpu
-        pos = d_pos |> cpu
-
-        Plots.scatter(pos[1,:], pos[2,:], aspect_ratio=:equal, legend=false)
-        quiver!(pos[1,:], pos[2,:], quiver=(0.02*cos.(θ), 0.02*sin.(θ)), color=:blue)
-
-        xlims!(0, 1)
-        ylims!(0, 1)
-
-        if τ % 100 == 0
-            println("animation step: $τ of $anim_size")
-        end
-    end
-
-    gif(anim, "./results/boid_animation.mp4", fps = 10)
-
-end
-
-function update_kernel!(d_i, d_r, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_speed, W₁, W₂, b₁, b₂, cm, x, out, avg_coord)
+function update_kernel!(d_params, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_speed, W₁, W₂, b₁, b₂, x, out, avg_coord)
     idx = threadIdx().x + (blockIdx().x - 1) * blockDim().x
 
-    box_size=1
-    Δt = 1e-2
-    β = 1.0
-    μ = 1e-5
+    nₚₐᵣₜₛ = Int(d_params[1])
+    Δt = d_params[3]
+    μ = d_params[4]
+    β = d_params[5]
+    κ = d_params[6]
+    γ = d_params[7]
+    x₀ = d_params[8]
+    box_size = d_params[9]
+    vision = d_params[10]
+    innertia = d_params[11]
+    avg_repr = d_params[12]
+    roulette = d_params[13]
+    potential = d_params[14]
+    fermionic = d_params[15]
+    weighted_repr = d_params[16]
+    reflect_walls = d_params[17]
+
     avg_θ = 0.0
     @inbounds d_τ[idx] += 1
 
     #Get direction from neural network
     ############################################################################################################################
-    for jdx in 1:length(d_i)
+    for jdx in 1:nₚₐᵣₜₛ
         if idx != jdx
             dist = 0.0
             @inbounds fov = (1.0 + cos(d_θ[idx] - d_θ[jdx])) #Field of vision
@@ -195,45 +44,46 @@ function update_kernel!(d_i, d_r, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_s
             end
             dist = sqrt(dist)
 
-            @inbounds avg_θ += ((exp(-β*dist^2.0)/length(d_i))) * fov * d_θ[jdx]
+            @inbounds avg_θ += ((exp(-β*dist^2.0)/nₚₐᵣₜₛ)) * fov * d_θ[jdx]
 
             for kdx in 1:2
-                @inbounds avg_coord[kdx] += ((exp(-β*dist^2)/length(d_i)) * fov) * d_pos[kdx,jdx]
+                @inbounds avg_coord[idx,kdx] += ((exp(-β*dist^2)/nₚₐᵣₜₛ) * fov) * d_pos[kdx,jdx]
             end
         end
     end
 
-    x[1] = avg_coord[1]
-    x[2] = avg_coord[2]
-    x[3] = avg_θ
+    x[idx,1] = avg_coord[idx,1]
+    x[idx,2] = avg_coord[idx,2]
+    x[idx,3] = avg_θ
 
     for jdx in 1:size(W₁,3)
         tmp = 0
         for kdx in 1:size(W₁,2)
             @inbounds tmp += W₁[idx,kdx,jdx] * x[kdx]
         end
-        out[jdx] = tmp
+        out[idx,jdx] = tmp
     end
 
     for jdx in 1:size(W₂,3)
         tmp = 0
         for kdx in 1:size(W₂,2)
-            @inbounds tmp += W₂[idx,kdx,jdx] * tanh(out[kdx] + b₁[idx,kdx])
+            @inbounds tmp += W₂[idx,kdx,jdx] * tanh(out[idx,kdx] + b₁[idx,kdx])
         end
-        out[jdx] = tmp
+        out[idx,jdx] = tmp
     end
-    @inbounds out[1] = out[1] + b₂[idx]
+    @inbounds out[idx,1] = out[idx,1] + b₂[idx]
+#     CUDA.@cuprint(out[1], "\n")
 
     ############################################################################################################################
 
     #Recalculate particles' coordinates x,y,θ
     ############################################################################################################################
-#     if innertia
+
     #BUG ### TEST TO SEE IF USING THE FIRST INDEX IS CORRECT
-    @inbounds d_θ[idx] = mod(d_θ[idx] + π*out[1] * Δt + π, 2.0 * π) - π
-#     @inbounds d_θ[idx] = mod(d_θ[idx] + π * out * Δt + π, 2.0 * π) - π
+#     if innertia
+#         @inbounds d_θ[idx] = mod(d_θ[idx] + π*out[1] * Δt + π, 2.0 * π) - π
 #     else
-#         d_coords[3,idx] = d_θ[idx]
+        @inbounds d_θ[idx] = π*out[idx,1]
 #     end
 
     @inbounds d_pos[1,idx] = d_pos[1,idx] + d_speed[idx] * Δt * cos(d_θ[idx])
@@ -242,13 +92,17 @@ function update_kernel!(d_i, d_r, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_s
 
     #Periodic boundary conditions
     ############################################################################################################################
+#     if !reflect_walls
     @inbounds d_pos[1,idx] = mod(d_pos[1,idx] + box_size, box_size)
     @inbounds d_pos[2,idx] = mod(d_pos[2,idx] + box_size, box_size)
+#     else
+
+#     end
     ############################################################################################################################
 
     #Birth Dynamics
     ############################################################################################################################
-    for jdx in 1:length(d_i)
+    for jdx in 1:nₚₐᵣₜₛ
         dist = 0.0
         for kdx in 1:2
             @inbounds dist += (d_pos[kdx,idx] - d_pos[kdx,jdx])^2
@@ -259,14 +113,14 @@ function update_kernel!(d_i, d_r, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_s
             #Death dynamics - Here I use a accumulated random selector for killing a particle
             ############################################################################################################################
             max_r = 0
-            for ldx in 1:length(d_ϕ)
+            for ldx in 1:nₚₐᵣₜₛ
                 max_r += d_ϕ[ldx]
             end
             r = max_r * rand()
 
             kdx = 0
             sum = 0
-            while sum <= r && kdx <= length(d_ϕ)-2
+            while sum <= r && kdx < nₚₐᵣₜₛ
                 kdx+=1
                 sum+=d_ϕ[kdx]
             end
@@ -324,20 +178,22 @@ function update_kernel!(d_i, d_r, d_τ, d_ϕ, d_cn, d_sx, d_rr, d_pos, d_θ, d_s
     return
 end
 
-# function order_parameter()
-#     v = [0,0]
-#     sum_speed = 0
-#     for boid in Boids
-#         v = v .+ boid.speed * [cos(boid.θ), sin(boid.θ)]
-#         sum_speed = sum_speed + boid.speed
-#     end
-#
-#     avg_speed = sum_speed/length(Boids)
-#
-#     ψ = norm(v)/(length(Boids)*avg_speed)
-#
-#     return ψ
-# end
+function order_parameter(nₚₐᵣₜₛ, d_pos, d_θ, d_speed)
+    vx = 0
+    vy = 0
+    sum_speed = 0
+
+    for idx in 1:nₚₐᵣₜₛ
+        vx += d_speed[idx] * cos(d_θ[idx])
+        vy += d_speed[idx] * sin(d_θ[idx])
+        sum_speed = sum_speed + d_speed[idx]
+    end
+    avg_speed = sum_speed/nₚₐᵣₜₛ
+
+    ψ = sqrt(vx^2 + vy^2)/(nₚₐᵣₜₛ*avg_speed)
+
+    return ψ
+end
 #
 # function collisions(Boids::Vector{Boid})
 #     #Number of particle collisions
